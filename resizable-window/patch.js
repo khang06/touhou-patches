8d869ef52673d04dfe4c35bebc724bdb1a6009fe
Khang <khang06@users.noreply.github.com>
2020-01-08 07:52:11 -0500

    maybe it will work now???

--- resizable-window/patch.js
+++ resizable-window/patch.js
@@ -1,7 +1,7 @@
 {
-    "id": "resizable-window",
-    "servers": [
-        "https://raw.githubusercontent.com/khang06/touhou-patches/master/resizable-window/"
-    ],
-    "title": "Pass different flags into CreateWindowExA to allow resizing and maximizing"
+	"id": "resizable-window",
+	"servers": [
+		"https://raw.githubusercontent.com/khang06/touhou-patches/master/resizable-window/"
+	],
+	"title": "Pass different flags into CreateWindowExA to allow resizing and maximizing"
 }
