fa4a14cf8bffd9e4b8ad63de6ae1af929f1cf107
Khang <khang06@users.noreply.github.com>
2020-01-08 07:41:21 -0500

    better description for resizable window

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "khanglegos+you.should.contact.my.discord.khangaroo.5062@gmail.com",
 	"id": "Khangaroo",
 	"patches": {
-		"resizable-window": "Resizable Window"
+		"resizable-window": "Pass different flags into CreateWindowExA to allow resizing and maximizing"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/khang06/touhou-patches/master/"
