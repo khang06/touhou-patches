d999fc00c72e6031806c9934182eee224effbc6f
Khang <khang06@users.noreply.github.com>
2024-10-18 10:38:48 -0400

    QuickLoad for WBaWC

---
+++ quick_load/th17.v1.00b.js
@@ -0,0 +1,26 @@
+{
+    binhacks: {
+        quick_load_hook: {
+            addr: "0x440FDF",
+            code: "E8[codecave:quick_load_cave]"
+        },
+        quick_load_no_title_load_wait: {
+            addr: "0x4511AF",
+            code: "<nop:2>"
+        },
+        quick_load_no_title_input_wait1: {
+            addr: "0x452204",
+            code: "<nop:6>"
+        },
+        quick_load_no_title_input_wait2: {
+            addr: "0x45200F",
+            code: "<nop:6>"
+        }
+    },
+    codecaves: {
+        quick_load_cave: {
+            access: "rx",
+            code: "53 8B5C2408 53 E8[0x4747F0] 83BB2801000000 75F1 5B C20400"
+        }
+    }
+}
