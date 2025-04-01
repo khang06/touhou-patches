f45222ac0ffde32676369042281257c589537de8
Khang <khang06@users.noreply.github.com>
2025-04-01 01:02:57 -0400

    Maybe fix the repo for real?

--- repo.js
+++ repo.js
@@ -2,11 +2,9 @@
 	"contact": "khanglegos+you.should.contact.my.discord.at.khangaroo@gmail.com",
 	"id": "Khangaroo",
 	"patches": {
-		"im_using_tilt_controls": "(TH14) ImUsingTiltControls: Gyro movement via Cemuhook UDP protocol",
-		//"multi_instance": "Allows you to run multiple instances of every game",
+		"im_using_tilt_controls": "(TH14) Gyro movement via Cemuhook UDP protocol",
 		"real_bullet_size_th06": "Same as nmlgc/real_bullet_size, but with EoSD support",
 		"real_bullet_size_th06_retexture": "Same as real_bullet_size_th06, but with support for the retexture",
-		//"resizable-window": "Pass different flags into CreateWindowExA to allow resizing and maximizing",
 		"ultra_port": "(TH10) thcrap port of NiL's ultra patch",
 		"um_chaos": "(TH18) Unconnected Marketeers Chaos Edition"
 	},
