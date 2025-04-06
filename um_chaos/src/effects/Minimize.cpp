9331d079a8604b654e881b0f7a41b8487ea1a19d
Khang <khang06@users.noreply.github.com>
2025-04-06 11:19:41 -0400

    UM Chaos: Never minimize in fullscreen

--- um_chaos/src/effects/Minimize.cpp
+++ um_chaos/src/effects/Minimize.cpp
@@ -7,5 +7,9 @@ public:
     Minimize() {
         PostMessage(Main::Window, WM_SYSCOMMAND, SC_MINIMIZE, 0);
     }
+    
+    virtual bool Allowed() {
+        return !Window::IsFullscreen();
+    }
 };
 REGISTER_EFFECT(Minimize);
