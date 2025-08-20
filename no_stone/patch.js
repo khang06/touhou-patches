1dfd64aed02ea0abc0f4ede7430b1e81e979514f
Khang <khang06@users.noreply.github.com>
2025-08-20 02:10:53 -0400

    No Stone: Initial commit

---
+++ no_stone/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"base_tsa"
+	],
+	"id": "no_stone",
+	"servers": [
+		"https://raw.githubusercontent.com/khang06/touhou-patches/master/no_stone/"
+	],
+	"title": "(TH20) No Stone"
+}
---
+++ no_stone/th20.v1.00a.js
@@ -0,0 +1,52 @@
+{
+    binhacks: {
+        no_stone_fix_vtbl_0x14: {
+            addr: "Rx175810+0x14",
+            code: "<Rx1301C0>",
+        },
+        no_stone_fix_vtbl_0x18: {
+            addr: "Rx175810+0x18",
+            code: "<Rx130370>",
+        },
+        no_stone_operator_new: {
+            addr: "Rx132385",
+            code: "E8[Rx12FAD0]",
+        },
+        no_stone_create_table: {
+            addr: "Rx1B0AB0",
+            code: "<Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460><Rx132460>"
+        },
+        no_stone_color_table: {
+            addr: "Rx174EC8",
+            code: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+        },
+        no_stone_swap_equip_anm: {
+            addr: "Rx63DEE",
+            code: "B808000000"
+        },
+        no_stone_swap_equip_copy_anm: {
+            addr: "Rx63E4C",
+            code: "B808000000 <nop:3>"
+        },
+        no_stone_no_options: {
+            addr: "RxFAECC",
+            code: "E9[RxFB21B]"
+        },
+        no_stone_no_hyper_duration: {
+            addr: "Rx11B8E0",
+            code: "31C0 C3"
+        },
+        no_stone_no_hyper_add: {
+            addr: "Rx133780",
+            code: "C20400"
+        },
+        no_stone_always_random_priority: {
+            addr: "RxA5A5E",
+            code: "EB"
+        },
+        no_stone_always_random_stone_item: {
+            addr: "RxC4458",
+            code: "EB"
+        }
+    }
+}
